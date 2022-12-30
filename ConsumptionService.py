0bba8a7e0ebf3a8c3c290e92a6be2f7367ea134f
JMF <65224186+JMFede@users.noreply.github.com>
2022-12-30 16:57:06 +0100

    Cazzo di repository

---
+++ ConsumptionService.py
@@ -0,0 +1,143 @@
+import paho.mqtt.client as PahoMQTT
+import datetime
+import time
+import requests
+import json
+import pandas as pd
+import os
+
+class ConsumptionManager:
+	def __init__(self):
+		"""Initialize the ConsumptionManager class"""
+
+		conf = json.load(open('ConsumptionService_settings.json')) 
+		self.clientID = conf['clientID'] #FROM SERVICE CATALOG self.ID
+		self.catalogIP = conf['catalogIP']
+		self.serviceName = conf['serviceName']
+
+		ServiceInfo = json.loads(requests.get(f'{self.catalogIP}/{self.serviceName}?ID={self.ID}')) # TO CHECK
+
+		self.broker = json.loads(requests.get("self.catalogIP/broker"))
+		self.port = ServiceInfo['port']
+		self.basicTopics = ServiceInfo['topic']
+
+		r = json.loads(requests.get(f'{self.catalogIP}/getCompanyList'))
+		self.CompanyList = self.updateCompanyList(r['CompanyList'])
+
+	def updateCompanyList(CompanyList : list):
+		"""Update the company list with the actuators status and consumption"""
+		for comp in CompanyList:
+			for dev in comp['deviceList']:
+				if dev['isActuator']:
+					dev.update({'companyName' : comp['companyName'], 'Datetime' : None, 'status': 'OFF', 'OnTime': 0, 'Consumption' : 0})
+		return CompanyList
+
+	def updateConsumption(self):
+		"""Calculate the consumption of the actuators for the passed hour and update the database"""
+		currentTime = datetime.datetime.now()
+		dframe_list = []
+		for comp in self.CompanyList:
+			actuators = []
+			for dev in comp['deviceList']:
+				if(dev['isActuator'] and dev['status'] == 'OFF'):
+					dev['Datetime']= f"{currentTime.date()} {currentTime.hour}:00:00"
+
+					actuators.append(dev.copy())
+
+					dev['Consumption'] = 0
+					dev['Datetime'] = None
+
+				elif(dev['isActuator'] and dev['status'] == 'ON'):
+					dev['Datetime']= f"{currentTime.date()} {currentTime.hour}:00:00"
+					dev['Consumption'] = round((time.time() - dev['OnTime'])*dev['PowerConsumption_kW']/3600,2)
+
+					actuators.append(dev.copy())
+
+					dev['OnTime'] = time.time()
+					dev['Consumption'] = 0
+					dev['Datetime'] = None
+
+			df = pd.DataFrame(actuators, columns=['companyName', 'ID', 'deviceName', 'PowerConsumption_kW', 'Datetime', 'Consumption'])
+			dframe_list.append(df)
+		df = pd.concat(dframe_list)
+		df.to_csv('Consumption.csv', index=False) #!!!!! PUT REQUEST TO DATABASE TODO
+
+	def upgradeStatus (self, companyName: str, actuatorID: str, command: str):
+		"""Update the status of the actuator, if it is turned OFF calculates its consumption"""
+		try:
+			for comp in self.CompanyList:
+				if comp['comapnyName'] == companyName:
+					for dev in comp:
+						if dev['deviceName'] == actuatorID:
+							if command == 'ON':
+								dev['status'] = command
+								dev['OnTime'] = time.time()
+							elif command == 'OFF':
+								dev['status'] = command
+								#Calculate the consumption of a actuator by its mean power consumption and the time it was on 
+								dev['Consumption'] += round((time.time() - dev['OnTime'])*dev['PowerConsumption_kW']/3600,2)
+								dev['OnTime'] = 0
+							else:
+								raise Exception("Wrong message")
+						return
+			raise Exception("Actuator or Company not found")
+		except Exception as e:
+			raise e
+
+class MQTTConsumptionManager(ConsumptionManager):
+	def __init__(self):
+		super().__init__()
+		self.topics = self.createTopics()
+		
+		self._paho_mqtt = PahoMQTT.Client(self.clientID, True)
+		self.start()
+		self._paho_mqtt.on_connect = self.OnConnect
+		self._paho_mqtt.on_message = self.MessageReceived
+
+	def createTopics(self):
+		"""Create the topics for each actuator of each company"""
+		topics = []
+		for comp in self.CompanyList:
+			for dev in comp['deviceList']:
+				if dev['isActuator']:
+					topics.append(f"{self.basicTopics}/{comp['companyName']}/{dev['ID']}")
+		return topics
+
+	def start (self):
+		"""Connect to the broker and subscribe to the topics"""
+		self._paho_mqtt.connect(self.broker, self.port)
+		self._paho_mqtt.loop_start()
+		# subscribe for a topic
+		for i in self.topics:
+			self._paho_mqtt.subscribe(i, 2)
+
+	def stop (self):
+		"""Disconnect from the broker and unsubscribe from the topics"""
+		for i in self.topics:
+			self._paho_mqtt.unsubscribe(i)
+		self._paho_mqtt.loop_stop()
+		self._paho_mqtt.disconnect()
+
+	def OnConnect (self, paho_mqtt, userdata, flags, rc):
+		print ("Connected to %s with result code: %d" % (self.broker, rc))
+
+	def MessageReceived (self, paho_mqtt , userdata, msg):
+		"""Parse the message received and control the topic"""
+		try:
+			command = list[msg.payload.values()][0]
+			head, deviceID = os.path.split(msg.topic)
+			topic, companyName = os.path.split(head)
+			if topic not in self.basicTopics:
+				raise ValueError("Topic not valid")
+			else:
+				self.upgradeStatus(companyName, deviceID, command)
+		except ValueError as e:
+			print(e)
+
+
+if __name__ == "__main__":
+	cm = MQTTConsumptionManager()
+	while True:
+		if datetime.now().minute >= 59: #Update the consumption every hour
+			cm.updateConsumption()
+		time.sleep(1)
---
+++ FaultDetService.py
@@ -0,0 +1,130 @@
+import paho.mqtt.client as PahoMQTT
+import datetime
+import time
+import requests
+import json
+import pandas as pd
+import os
+
+class FaultDetector:
+	def __init__(self):
+		"""Initialize the FaultDetector class"""
+
+		conf = json.load(open('FaultDetectionService_settings.json')) 
+		self.clientID = conf['clientID'] #FROM SERVICE CATALOG self.ID
+		self.catalogIP = conf['catalogIP']
+		self.serviceName = conf['serviceName']
+
+		ServiceInfo = json.loads(requests.get(f'{self.catalogIP}/{self.serviceName}?ID={self.ID}')) # TO CHECK
+
+		self.broker = json.loads(requests.get("self.catalogIP/broker"))
+		self.port = ServiceInfo['port']
+		self.basicTopic = ServiceInfo['topic']
+
+		r = json.loads(requests.get(f'{self.catalogIP}/getCompanyList'))
+		CompanyList = self.updateCompanyList(r['CompanyList'])
+		self.deviceList = self.createDeviceList(CompanyList)
+		self.thresholds = {
+							'temperature' : {'max_value': 4000,'min_value': -100, 'unit': '°C'},
+						 	'humidity' : {'max_value': 90, 'min_value': 0, 'unit': '%'},
+						  	'light' : {'max_value': 1000, 'min_value': 10**(-5), 'unit': 'lx'}
+						  }
+		
+	def createDeviceList(CompanyList : list):
+		"""Create a list of all devices integrating informations about the last time a message was received from a device"""
+		deviceList = []
+		for comp in CompanyList:
+			for dev in comp['deviceList']:
+				if(dev['isSensor']):
+					deviceList.append(dev.copy().update({'companyName' : comp['companyName'], 'LastUpdate' : None}))
+		return deviceList
+
+	def updateDeviceStatus(self, deviceID : str):
+		"""Update the status of a device in the deviceList"""
+		try:
+			for dev in self.deviceList:
+				if dev['ID'] == deviceID:
+					dev['LastUpdate'] = datetime.datetime.now()
+					break
+		except:
+			raise Exception("Device not found")
+
+	def checkDeviceStatus(self,):
+		"""Check if a device has not sent a message for more than 5 minutes"""
+		currentTime = datetime.datetime.now()
+		try:
+			for dev in self.deviceList:
+				if dev['LastUpdate'] is not None:
+					if (currentTime - dev['LastUpdate']).total_seconds() > 300:
+						# Send a message to the users of the respective company to notify the fault
+						# TO DO
+						pass
+		except:
+			raise Exception("Device not found")
+
+	def checkMeasure(self, deviceID: str, measureType: str,  measure : dict):
+		"""Check if a measure is within the thresholds"""
+		try:
+			for dev in self.deviceList:
+				if dev['ID'] == deviceID:
+					if measureType in dev['measureType']:
+						if measure > self.thresholds[measureType]['max_value'] or measure < self.thresholds[measureType]['min_value']:
+							# Send a message to the users of the respective company to notify the fault
+							# TO DO
+							pass
+						break
+		except:
+			raise Exception("Device not found")
+		return True
+
+
+class MQTTFaultDetector(FaultDetector):
+	def __init__(self):
+		super().__init__()
+		self.topics = self.createTopics()
+		
+		self._paho_mqtt = PahoMQTT.Client(self.clientID, True)
+		self.start()
+		self._paho_mqtt.on_connect = self.OnConnect
+		self._paho_mqtt.on_message = self.MessageReceived
+
+	def createTopics(self):
+		"""Create the topics for each actuator of each company"""
+		topics = []
+		for dev in self.deviceList:
+			for measure in dev['measureType']:
+				topics.append(f"{self.basicTopic}/{dev['companyName']}/{dev['ID']}/{measure}")
+		return topics
+
+	def start (self):
+		"""Connect to the broker and subscribe to the topics"""
+		self._paho_mqtt.connect(self.broker, self.port)
+		self._paho_mqtt.loop_start()
+		# subscribe for a topic
+		for i in self.topics:
+			self._paho_mqtt.subscribe(i, 2)
+
+	def stop (self):
+		"""Disconnect from the broker and unsubscribe from the topics"""
+		for i in self.topics:
+			self._paho_mqtt.unsubscribe(i)
+		self._paho_mqtt.loop_stop()
+		self._paho_mqtt.disconnect()
+
+	def OnConnect (self, paho_mqtt, userdata, flags, rc):
+		print ("Connected to %s with result code: %d" % (self.broker, rc))
+
+	def MessageReceived (self, paho_mqtt , userdata, msg):
+		"""Parse the topic received"""
+		topic_list = msg.topic.split('/')
+		measureType = topic_list[-1]
+		deviceID = topic_list[-2]
+		measure = msg.payload['measure']
+		self.checkMeasure(deviceID, measureType, measure)
+		self.updateDeviceStatus(deviceID)
+
+if __name__ == "__main__":
+	fd = MQTTFaultDetector()
+	while True:
+		fd.checkDeviceStatus()
+		time.sleep(5)

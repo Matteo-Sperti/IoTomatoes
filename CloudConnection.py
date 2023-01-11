dd3d818fdd2d3b15c3e9bbb57e7e1b0c24fa8607
Luca Zanetti <luca.zanett.99@gmail.com>
2023-01-12 00:54:03 +0100

    fixed CloudConnection structure

--- CloudConnection.py
+++ CloudConnection.py
@@ -1,54 +1,51 @@
 from pymongo import MongoClient
+from pymongo import errors
 import json
 import time
 #from GenericEndpoint import GenericEndpoint
 import requests
 
-
 class MongoConnection():
     def __init__(self):
-        self.client = MongoClient("mongodb+srv://admin:admin@cluster0.lzvxrr9.mongodb.net/test")
+        try:
+            self.client = MongoClient("mongodb+srv://admin:admin@cluster0.lzvxrr9.mongodb.net/test")
+        except errors.AutoReconnect():
+            print("Error connecting to the database")
         #super().__init__()
+        #self.checkNewCompany()
     
     def insertDataBase(self, CompanyName):
         '''create a new database (company)
          Arguments:
-            CompanyName: unique name of the company'''
-        if CompanyName in self.client.list_database_names():
-            print("Database already exists")
-        else:
+         CompanyName: unique name of the company'''
+        if CompanyName not in self.client.list_database_names():
             db = self.client[CompanyName]
             collection = db["CompanyData"]
             data = {"Company": CompanyName, "Database Creation Time": time.ctime()}
             collection.insert_one(data)
-            print("Database created")
-    def insertCollection(self, CompanyName, CollectionName,data):
-        '''create a new collection (dataset for a company)/update a collection
+            print("Database created") 
+        else:
+            print("Database already exists")
+
+    def insertField(self, CompanyName, CollectionName,data):
+        '''create a collection for a field (dataset for a company)/update a collection\n
             Arguments:
-            CompanyName: unique name of the company
-            CollectionName: unique name of the collection
-            data: data to be inserted in the collection'''
+            `CompanyName`: unique name of the company\n
+            `CollectionName`: unique name of the collection\n
+            `data`: data to be inserted in the collection\n'''
         if CompanyName in self.client.list_database_names():
-            db = self.client[CompanyName]
-            update_flag = False
-            if CollectionName in self.client[CompanyName].list_collection_names():
-                update_flag = True
             db = self.client[CompanyName]
             collection = db[CollectionName]
             data["_id"]=data.pop("bn")
             collection.insert_one(data)
-            if update_flag:
-                print("Collection updated")
-            else:
-                print("Collection created")
-
         else:
-            print("Database does not exist, please create database first")
-    def sensorData(self, CompanyName, CollectionName,ID,data):
+            print("Company database is still being generated")
+    def insertDataSensors(self, CompanyName, CollectionName,ID,data):
         '''insert data in a collection\n
             Parameters:
             CompanyName: unique name of the company
             CollectionName: unique name of the collection
+            ID of the device
             data: data to be inserted in the collection'''
         if CompanyName in self.client.list_database_names():
             if CollectionName in self.client[CompanyName].list_collection_names():
@@ -56,7 +53,6 @@ class MongoConnection():
                 collection = db[CollectionName]
                 data["_id"]=data.pop("bn")
                 dict =list(collection.find())
-                
                 for i in range(len(data["e"])):
                     for j in range(len(dict[ID]["e"])):
                         if data["e"][i]["name"] == dict[ID]["e"][j]["name"]:
@@ -65,41 +61,37 @@ class MongoConnection():
                             temp=dict[ID]["e"][j]["value"].extend([data["e"][i]["value"]])
                             collection.update_one({"_id":ID}, {"$set": dict[ID]})
             else:
-                   print("Collection does not exist, please create collection first")
+                   self.insertField(CompanyName,CollectionName,data)
         else:
             print("Database does not exist, please create database first")
-            
-    def UpdateField(self,LocationOfTheValues,FieldToUpdate):
-        '''update a field in a collection\n
-            Parameters:\n
-            LocationOfTheValues: location of the values to be updated\n
-            FieldToUpdate: field to be updated'''
-        #da fare
+    
+    def insertDataService(self,CompanyName,CollectionName,data):       
+    #da fare
                          
-    # def notify(self,topic, payload):
-    #         '''get data on notification
-    #         Parameters:
-    #         notification: string containing the topic of the new message'''
-    #         print(f"new message in topic: {topic}")
-    #         list=topic.split("/")
-    #         self.sensorData(list[1],list[2],payload)
+    def notify(self,topic, payload):
+             '''get data on notification
+            Parameters: \n
+            `topic` -- string containing the topic of the new message\n
+            `payload` -- string containing the payload of the new message\n'''
+    
+             list=topic.split("/")
+             self.insertDataSensors(list[1],list[2],payload)
             
             #need how to get data from topic to right collection
             
+        
+    def checkNewCompany(self):
+            '''check if a new company is added by making a GET request to the Resource Catalog'''
+            while True:
+                response = requests.get(self.ResourceCatalog_url + "/CompaniesName", self._SystemToken)
+                list = json.loads(response)
+                for i in list:
+                    if i not in self.client.list_database_names():
+                        self.insertDataBase(i)
+                time.sleep(300)
             
+                
 
-        
-    #     '''get data when notify happens'''
-    # def checkNewCompany(self,CatalogUrl):
-    #         '''check if a new company is added by making a GET request to the Resource Catalog'''
-    #         response = requests.get(self.ResourceCatalog_url + "/CompaniesName", self._SystemToken)
-    #         list = json.loads(response)
-    #         for i in list:
-    #             if i not in self.client.list_database_names():
-    #                 self.insertDataBase(i)
-        
-            
-    #     '''check if a new company is added'''
     # def GetBotGrafici
     # def Getmedia
     '''da decidere se inserire o meno'''
@@ -108,6 +100,7 @@ if __name__ == '__main__':
     #mongo.insertDataBase("Google")
     #print(mongo.client.list_database_names())
     #mongo.insertCollection("Google","Lighting",json.load(open("provajson.json","r")))
-    mongo.sensorData("Google","Lighting",1,json.load(open("provajson.json","r")))
+    #mongo.insertDataSensors("Google","Lighting",2,json.load(open("provajson.json","r")))
     company="Google"
+    mongo.insertDataBase("Google")
    # print(list(mongo.client[company]["Lighting"].find()))

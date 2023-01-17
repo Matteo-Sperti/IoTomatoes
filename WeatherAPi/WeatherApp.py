bc6e2c4ded7890e68d7104c01ead7a101be6d6ff
Luca Zanetti <luca.zanett.99@gmail.com>
2023-01-17 22:52:59 +0100

    final version of the weather api

--- WeatherAPi/WeatherApp.py
+++ WeatherAPi/WeatherApp.py
@@ -2,6 +2,8 @@ import requests
 import json
 import time
 import cherrypy
+from GenericEndpoint import GenericEndpoint
+from MyExceptions import *
 
 
 def validateJSON(jsonData):
@@ -14,20 +16,20 @@ def validateJSON(jsonData):
 
 class WeatherApp:
 	'''handles the weather requests'''
-	def __init__(self):
-		self.url= "https://api.open-meteo.com/v1/forecast"
-	def makeRequest(self,fileInput,fileOutput):
+	def __init__(self,settings):
+		self.settings = settings
+		self.url = self.settings["WeatherUrl"]
+	def makeRequest(self,dictInput):
 		'''handles an API request based on the JSON input file(forwards a getRequest to the weather API)'''
-		dict = json.load(open(fileInput,"r")) #loads the input file
-		response = requests.get(self.url, params=dict) #makes the request, the parameters are in the input file
-		return response.json()
+		response = requests.get(self.url, params=dictInput) #makes the request, the parameters are in the input file
+		return response
 
 	def IrrigationData(self):
 		'''gets the data from the weather API for the irrigation service'''
-		return self.makeRequest("IrrigationInput.json","IrrigationOutput.json") #makes the request and writes the output in the output file
+		return self.makeRequest(self.settings["IrrigationDict"]) #makes the request 
 	def LightingData(self):
 		'''gets the data from the weather API for the lighting service'''
-		response = self.makeRequest("LightingInput.json","LightingOutput.json")
+		response = self.makeRequest(self.settings["LightingDict"]) #makes the request
 		listToBeConverted=(response["hourly"].pop("shortwave_radiation"))
 		convertedList=[x/0.0079 for x in listToBeConverted] #converts the shortwave radiation to lux
 		response["hourly"]["Illumination"]=convertedList
@@ -38,26 +40,28 @@ class WeatherApp:
 		response["hourly_units"].pop("shortwave_radiation")
 		response["daily_units"]["Illumination_sum"]="lux"
 		response["daily_units"].pop("shortwave_radiation_sum")
-		return response #makes the request and writes the output in the output file
-	def CustomData(self):
-		'''gets the data from the weather API for a  custom user request'''
-		return self.makeRequest("CustomInput.json","CustomOutput.json") #makes the request and writes the output in the output file
+		return response #makes the 
+	# def CustomData(self):
+	# 	'''gets the data from the weather API for a  custom user request'''
+	# 	return self.makeRequest(self.settings["CustomDict"]) for now deprecated
 
 
 
-class WebPage(object):
+class WebPage(GenericEndpoint):
 	exposed = True
+	def __init__(self,settings):
+		
+		
+		super().__init__(settings,True,False)
+		self.weather= WeatherApp(self.settings,True,False)
 	def GET(self,*uri,**params):
 		if len(uri) != 0:
 			if uri[0] == "Irrigation":
-				weather = WeatherApp()
-				return json.dumps(weather.IrrigationData())
+				return json.dumps(self.weather.IrrigationData())
 			elif uri[0] == "Lighting":
-				weather = WeatherApp()
-				return json.dumps(weather.LightingData())
+				return json.dumps(self.weather.LightingData())
 			elif uri[0] == "Custom":
-				weather = WeatherApp()
-				data = json.dumps(weather.CustomData())
+				data = json.dumps(self.weather.CustomData())
 				if data == "Null":
 					raise cherrypy.HTTPError(400, "Wrong request")
 				elif validateJSON(data) == False:
@@ -75,16 +79,19 @@ class WebPage(object):
 	
 
 if __name__ == '__main__':
+	
+	settings=json.load(open("settings.json","r"))
+	
 	try:
 
 		print("Starting server...")
 		conf =  {
 		 '/': {
-		      'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-		      'tools.sessions.on': True,
+			  'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+			  'tools.sessions.on': True,
 		 	}
 		 		}
-		webService = WebPage()
+		webService = WebPage(settings)
 		cherrypy.tree.mount(webService, '/', conf)
 		cherrypy.engine.start()
 		cherrypy.engine.block() 

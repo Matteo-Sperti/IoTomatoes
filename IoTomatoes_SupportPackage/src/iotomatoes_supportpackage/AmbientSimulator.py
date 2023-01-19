9f2aa42862231b0257b0c5c8d45ff1ba37ef2b85
Matteo Sperti <matteo99.sperti@gmail.com>
2023-01-19 15:09:12 +0100

    bisogna aggiornare ambient simulator

--- IoTomatoes_SupportPackage/src/iotomatoes_supportpackage/AmbientSimulator.py
+++ IoTomatoes_SupportPackage/src/iotomatoes_supportpackage/AmbientSimulator.py
@@ -11,7 +11,7 @@ class AmbientSimulator():
 
         self._temperature = 20
         self._humidity = 50
-        self._light = 100
+        self._light = 50000
         self._soilMoisture = 50
         self._led = False
         self._pump = False
@@ -27,18 +27,30 @@ class AmbientSimulator():
             to the state of the actuators"""
 
         if self._led:
-            self._light = 150 + 10*self.noiseValue()
+            self._light = self._light + 1000 + 100*self.noiseValue()
         else:
-            self._light = 40 + 10*self.noiseValue()
+            self._light = self._light - 1000 - 100*self.noiseValue()
 
         if self._pump:
-            self._soilMoisture += (2 + self.noiseValue())
-            self._humidity += (1 + 0.2*self.noiseValue())
+            self._soilMoisture += (0.5 + 0.02*self.noiseValue())
+            self._humidity += (0.1 + 0.02*self.noiseValue())
         else:
-            self._soilMoisture -= (0.5 + self.noiseValue())
-            self._humidity -= (0.1 + 0.1*self.noiseValue())
+            self._soilMoisture -= (0.05 + 0.01*self.noiseValue())
+            self._humidity -= (0.01 + 0.01*self.noiseValue())
 
         self._temperature += (0.1 + 0.1*self.noiseValue())
+
+        self._soilMoisture = self.saturate(self._soilMoisture, 0, 100)
+        self._humidity = self.saturate(self._humidity, 0, 100)
+        self._light = self.saturate(self._light, 0, 100000)
+
+    def saturate(self, value, min, max):
+        """Saturate the value between min and max"""
+        if value > max:
+            value = max
+        elif value < min:
+            value = min
+        return value
 
     def get_temperature(self):
         return self._temperature + self.noiseValue()
